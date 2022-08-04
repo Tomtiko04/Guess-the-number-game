35311e3ab4be72950382a8da5358a233e463b0e3
Tomtiko04 <83877877+Tomtiko04@users.noreply.github.com>
2022-08-04 21:17:14 +0100

    Update script.js

--- Guess-Game/html/script.js
+++ Guess-Game/html/script.js
@@ -54,7 +54,6 @@ document.querySelector('.again').addEventListener('click', function() {
 
 /*console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = "🎉 Correct number!";
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+
 document.querySelector('.guess').value = 11;
 console.log(document.querySelector('.guess').value = 11);*/
