583a2e6d398879878a2c8aa2e2a766f7a72d4802
Ogunneye <83877877+Tomtiko04@users.noreply.github.com>
2022-08-18 16:42:45 +0100

    Update script.js

--- Guess-Game/html/script.js
+++ Guess-Game/html/script.js
@@ -14,6 +14,10 @@ let score = 20;
 let highscore = 0;
 console.log(secretNumber);
 
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+};
+
 //THE CHECK CLICK EVENT
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
